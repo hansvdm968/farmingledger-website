1b2b0a695720d08eef4eaf6ec7b5640bb8733f12
hansvdm968 <hansvdm62@gmail.com>
2026-06-01 14:44:45 +0200

    Release app 1.0.17 privacy hardening

--- app/flutter_bootstrap.js
+++ app/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2364868672" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "266356687" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
