bdc3aab7ae7d59b9f5636e2ca053abe0eed131ea
hansvdm968 <hansvdm62@gmail.com>
2026-06-02 12:13:20 +0200

    Publish app update 1.0.19

--- app/flutter_service_worker.js
+++ app/flutter_service_worker.js
@@ -1,10 +1,26 @@
-self.addEventListener('install', (event) => self.skipWaiting());
-self.addEventListener('activate', (event) => {
-  event.waitUntil((async () => {
-    const keys = await caches.keys();
-    await Promise.all(keys.map((key) => caches.delete(key)));
-    await self.registration.unregister();
-    const clientsList = await self.clients.matchAll({ type: 'window' });
-    clientsList.forEach((client) => client.navigate(client.url));
-  })());
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
 });
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    (async () => {
+      const cacheNames = await caches.keys();
+      await Promise.all(cacheNames.map((cacheName) => caches.delete(cacheName)));
+
+      if (self.registration.unregister) {
+        await self.registration.unregister();
+      }
+
+      const clientsList = await self.clients.matchAll({
+        includeUncontrolled: true,
+        type: "window",
+      });
+      for (const client of clientsList) {
+        client.navigate(client.url);
+      }
+    })(),
+  );
+});
+
+self.addEventListener("fetch", () => {});
