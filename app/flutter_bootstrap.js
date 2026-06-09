8173bf19995e330085baa1e24c3d3585c14e3386
hansvdm968 <hansvdm62@gmail.com>
2026-06-09 22:33:16 +0200

    Simplify farm module navigation

--- app/flutter_bootstrap.js
+++ app/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1012628764" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2655293401" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
