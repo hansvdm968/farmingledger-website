ef58902b70884b50c1ad7861ba0e9a360ef6d038
hansvdm968 <hansvdm62@gmail.com>
2026-07-01 06:33:35 +0200

    Publish scanner printed tag fix

--- app/flutter_bootstrap.js
+++ app/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3958921253" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3777472744" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
