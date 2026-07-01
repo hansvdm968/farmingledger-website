1ccad1678830a382f957e863f0dc718b5d7f3611
hansvdm968 <hansvdm62@gmail.com>
2026-07-01 16:51:58 +0200

    Publish 1.0.35 mobile fixes

--- app/flutter_bootstrap.js
+++ app/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "754661213" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1834685646" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
